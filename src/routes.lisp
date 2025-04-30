cd2163a3434bdcf164125f32e047bb24a2fec635
Matthew Bestard <matthew.r.bestard@gmail.com>
2025-04-30 12:43:07 -0400

    Added basic routes for main website pages

--- src/routes.lisp
+++ src/routes.lisp
@@ -5,9 +5,18 @@
 (tiny-routes:define-routes *app-routes*
   (tiny-routes:define-get "/" ()
     (tiny-routes:ok (render-index-page)))
+  (tiny-routes:define-get "/services" ()
+    (tiny-routes:ok (render-services-page)))
+  (tiny-routes:define-get "/about" ()
+    (tiny-routes:ok (render-about-page)))
+  (tiny-routes:define-get "/testimonials" ()
+    (tiny-routes:ok (render-testimonials-page)))
+  (tiny-routes:define-get "/contact" ()
+    (tiny-routes:ok (render-contact-page)))
   (tiny-routes:define-any "*" ()
     (tiny-routes:not-found "not found")))
 
 ;; Route Dispatcher
 (defun dispatch-routes (env)
   (funcall *app-routes* env))
+
--- src/views.lisp
+++ src/views.lisp
@@ -4,11 +4,40 @@
 ;; Define template variables
 (defparameter +base-template+ (djula:compile-template* "layout.html"))
 (defparameter +index-template+ (djula:compile-template* "index.html"))
+(defparameter +services-template+ (djula:compile-template* "services.html"))
+(defparameter +about-template+ (djula:compile-template* "about.html"))
+(defparameter +testimonials-template+ (djula:compile-template* "testimonials.html"))
+(defparameter +contact-template+ (djula:compile-template* "contact.html"))
 
 ;; Page rendering functions
 (defun render-index-page ()
   (djula:render-template* +index-template+ nil
 			  :title "The Steward"
-			  :now "2025"))
+			  :active "home"
+			  :now (local-time:format-timestring nil (local-time:now) :format '(:year))))
+
+(defun render-services-page ()
+  (djula:render-template* +services-template+ nil
+			  :title "The Steward"
+			  :active "services"
+			  :now (local-time:format-timestring nil (local-time:now) :format '(:year))))
+
+(defun render-about-page ()
+  (djula:render-template* +about-template+ nil
+			  :title "The Steward"
+			  :active "about"
+			  :now (local-time:format-timestring nil (local-time:now) :format '(:year))))
+
+(defun render-testimonials-page ()
+  (djula:render-template* +testimonials-template+ nil
+			  :title "The Steward"
+			  :active "testimonials"
+			  :now (local-time:format-timestring nil (local-time:now) :format '(:year))))
+
+(defun render-contact-page ()
+  (djula:render-template* +contact-template+ nil
+			  :title "The Steward"
+			  :active "contact"
+			  :now (local-time:format-timestring nil (local-time:now) :format '(:year))))
 
 
